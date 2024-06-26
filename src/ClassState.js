c514f68c3f7ad69210df69db63fa50e77cf722ed
dr1602 <david.arangelg@gmail.com>
2024-06-26 13:32:29 -0600

    Manejo de Estado con Clases 26jun24

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,16 +1,37 @@
+// un solo estado donde viven todos nuestros estados
 import React from 'react';
 
 class ClassState extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            error: false,
+        };  
+    }
+
     render() {
         return (
             <>
-                <h2> Eliminar ClassState </h2>
+                <h2> Eliminar {this.props.name} </h2>
 
                 <p> Escribe el código de seguridad para eliminar. </p>
 
+                {this.state.error && (
+                    <>
+                        <p> Error: El código es incorrecto. </p>
+                    </>
+                )}
+
                 <div>
                     <input placeholder='Código de seguridad'/>
-                    <button> Comprobar </button>
+                    <button
+                        // onClick={() => this.setState({ error: !this.state.error}) }
+                        onClick={() => this.setState( prevState => ({ error: !prevState.error }) )}
+                    > 
+                        Comprobar 
+                    </button>
                 </div>
             </>
         );
--- src/UseState.js
+++ src/UseState.js
@@ -1,15 +1,29 @@
-import React from 'react';
+// creacion de la logica de forma imperativa, se pueden crear varios estados
+import React, { useState } from 'react';
 
-const UseState = () => {
+const UseState = ({ name }) => {
+
+    const [error, setError] = useState(false)
+
     return (
         <>
-            <h2> Eliminar UseState </h2>
+            <h2> Eliminar {name} </h2>
 
             <p> Escribe el código de seguridad para eliminar. </p>
 
+            {error && (
+                <>
+                    <p> Error: El código es incorrecto. </p>
+                </>
+            )}
+
             <div>
                 <input placeholder='Código de seguridad'/>
-                <button> Comprobar </button>
+                <button
+                    onClick={() => setError(!error)}
+                > 
+                    Comprobar
+                </button>
             </div>
         </>
     );
